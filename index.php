a41acfcd96f8ee64228d3d721c0b4d2b3c64e76e
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-08-29 16:59:50 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Enhance Your Legal Journey with The Court Direct</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: justify;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+    <h1>Enhance Your Legal Journey with The Court Direct</h1>
+        <p>The Court Direct is your comprehensive directory for navigating the legal landscape in the USA. From court locations to legal resources, we provide the tools you need. Empower your legal journey with accurate information and support.</p>
+        <p>For more information, explore the <a href="https://thecourtdirect.com">Superior Court of Los Angeles</a>.</p>
+
+        <div>
+        </div>
+        <a href="https://www.facebook.com/ahsanghaffar23/" target="_blank"><button>Connect on Facebook</button></a>
+
+    </div>
+</body>
+
+</html>
